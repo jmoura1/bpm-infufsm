44ffdb7bffc9dd069328da3813e021e3a1999035
andrea <andrea@inf.ufsm.br>
2012-08-26 12:50:51 +0000

    Alteracoes em variaveis e scripts que tratam de decisoes (indeferido/aprovado)

---
+++ processes/trunk/BOS-5.7.2/studio/workspace/local_default_My Extensions/src-groovyScripts/Solicitar_ACG_0_1/DecisaoChoices.groovy
@@ -0,0 +1,5 @@
+package Solicitar_ACG_0_1;
+public class DecisaoChoices{
+public static final String _$_decisaoAprovado_= '${decisaoAprovado}';
+public static final String _$_decisaoIndeferido_= '${decisaoIndeferido}';
+}
